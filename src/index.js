9f0418deecd8bea3c48ede95a01d5c2f8fcec3cb
Jonathan Chhabra <Sonicrida@users.noreply.github.com>
2017-05-19 17:02:30 -0400

    Hook up proper version of react router with basic routing

--- src/index.js
+++ src/index.js
@@ -1,14 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, browserHistory } from 'react-router';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import ApolloClient, { createNetworkInterface } from 'apollo-client'
 import { ApolloProvider } from 'react-apollo'
 import App from './App';
+import JobPost from './Components/JobPost'
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
+const networkInterface = createNetworkInterface({
+  uri: process.env.REACT_APP_GRAPHQL_ENDPOINT
+});
+
+const client = new ApolloClient({
+  networkInterface
+});
+
 ReactDOM.render(
-  <App />,
+  <ApolloProvider client={client}>
+    <Router>
+      <div>
+        <Route exact path='/' component={App} />
+        <Route path='/job-post/:id' component={JobPost} />
+      </div>
+      
+    </Router>
+  </ApolloProvider>,
   document.getElementById('root')
 );
 registerServiceWorker();
