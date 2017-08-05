1e6cb8f760141fac59a727434ba8dcb8cf8216e9
Jonathan Chhabra <Sonicrida@users.noreply.github.com>
2017-08-05 12:18:32 -0400

    Comment out unused css and format nav bar jsx

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -4,27 +4,25 @@ import styled from 'styled-components';
 
 function Nav() {
     return (
-        <nav className="navbar is-transparent has-text-white container">
-
-            <div className="navbar-brand">
-                <NavLink exact className="navbar-item" activeClassName='active' to='/'>
-                    React Job Board
-                </NavLink>
-            </div>
-
-            <div className="navbar-menu">
-                <div className="navbar-start">
-                    <NavLink className="navbar-item" activeClassName='active' to='/create-job-post'>
-                        Post A Job
+        <nav className="navbar is-transparent">
+            <div className="container">
+                <div className="navbar-brand">
+                    <NavLink exact className="navbar-item" activeClassName="active" to="/">
+                        React Job Board
                     </NavLink>
                 </div>
-                    
+
+                <div className="navbar-menu">
+                    <div className="navbar-start">
+                        <NavLink className="navbar-item" activeClassName="active" to="/create-job-post">
+                            Post A Job
+                        </NavLink>
+                        <a className="navbar-item" activeClassName="active" to="/create-job-post">
+                            About
+                        </a>
+                    </div>
+                </div>
             </div>
-
-            
-
-
-
         </nav>
     );
 }
